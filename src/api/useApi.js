825228c096bc9dcd47e43fcd11d8a1874c1342ce
JVVVK <juozas.venskutonis@vvk.lt>
2019-06-04 21:23:47 +0300

    .

--- src/api/useApi.js
+++ src/api/useApi.js
@@ -2,23 +2,6 @@ import 'whatwg-fetch';
 import getBaseUrl from './baseUrl';
 
 const baseUrl = getBaseUrl();
-//const proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-
-/*export function getUsers(){
-    return get('users');
-}
-
-export function deleteUser(id) {
-    return del(`users/${id}`);
-}
-
-export function postData(a, b, c, d){
-    return post('data', a, b, c, d);
-}
-
-export function postData(){
-    return post('data');
-}*/
 
 export function getData(a,b,c,d){
      var qstring = 'data?viet_sk='+a+'&e_obj_sk='+b+'&k_obj_sk='+c+'&n_obj_sk='+d;
@@ -28,35 +11,6 @@ export function getData(a,b,c,d){
 function get(url){
     return fetch(baseUrl + url).then(onSuccess, onError);
 }
-
-/*function post(url){
-    var request = new Request(baseUrl + url, {
-        method: 'POST',
-        headers: {
-            "Access-Control-Allow-Origin":"*",
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            apiCall: {
-                viet_sk: 100,
-                e_obj_sk: 100,
-                k_obj_sk: 10,
-                n_obj_sk: 15
-            }
-        })
-    });
-    return fetch(request).then(onSuccess, onError);
-}*/
-
-// fuction name "delete" is a reserved word
-/*function del(url){
-   const request = new Request(baseUrl + url, {
-        method: 'DELETE'
-    });
-
-    return fetch(request).then(onSuccess, onError);
-}*/
 
 function onSuccess(response){
     return response.json();
--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import './index.css';
-//import numeral from "numeral";
-//import {getUsers, deleteUser, getData} from './api/userApi.js';
 import {getData} from './api/useApi.js';
-//import {postData} from './api/userApi.js';
 import * as Sentry from '@sentry/browser';
 
 Sentry.init({ dsn: 'https://07fc76f907ab453390ea6e39d1cf32e8@sentry.io/1414822' });
 
 window.runAPI =  function() {
+    global.document.getElementById('clients').innerHTML = "Klientų skaičius: ";
+    global.document.getElementById('time').innerHTML = "Skaičiavimo laikas (sek.): ";
     var viet_sk = document.getElementById("viet_sk").value;
     var e_obj_sk = document.getElementById("e_obj_sk").value;
     var k_obj_sk = document.getElementById("k_obj_sk").value;
     var n_obj_sk = document.getElementById("n_obj_sk").value;
-    //console.log(viet_sk);
     getData(viet_sk, e_obj_sk, k_obj_sk, n_obj_sk).then(function(result){
-    //getData().then(function(result){
         var data  = JSON.stringify(result);
+        var clients = result.klientai;
+        var time = result.laikas;
+        global.document.getElementById('clients').innerHTML += clients;
+        global.document.getElementById('time').innerHTML += time;
         global.document.getElementById('data').innerHTML = data;
-        //window.map.remove(result);
-        //var layer = JSON.parse('{"id":"sprendinys","type":"symbol","source":{"type":"geojson","data":{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[54.683,25.317]},"properties":{"icon":"marker"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[54.9,23.9]},"properties":{"icon":"marker"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[55.717,21.117]},"properties":{"icon":"marker"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[54.95,25.067]},"properties":{"icon":"marker"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[55.733,24.35]},"properties":{"icon":"marker"}}]}},"layout":{"icon-image":"{icon}-15"},"klientai":521258,"laikas":0}');
-        //window.map.addLayer('sprendinys');
-        //global.document.getElementById('mapFrame').reload();
         var mapLayer = window.map.getLayer('sprendinys');
             if(typeof mapLayer !== 'undefined') {
              // Remove map layer & source.
@@ -29,47 +26,6 @@ window.runAPI =  function() {
         window.map.addLayer(result);
     });
 };
-
-/* getData().then(function(result){
-    var data  = JSON.stringify(result);
-    global.document.getElementById('data').innerHTML = data;
-});
-
-/* const appValue = numeral(1000).format('0,0.00');
-//debugger;
-console.log(`I would pay ${appValue} Eur. for this awesome app!`); // eslint-disable-line no-console
-
-//Populate table of users via API call.
- getUsers().then(function(result){
-    let usersBody = "";
-
-    result.forEach(function(user){
-        usersBody+= `<tr>
-        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-        <td>${user.id}</td>
-        <td>${user.firstName}</td>
-        <td>${user.lastName}</td>
-        <td>${user.email}</td>
-        </tr>`
-    });
-
-    global.document.getElementById('users').innerHTML = usersBody;
-
-    const deleteLinks = global.document.getElementsByClassName('deleteUser');
-
-    //Must use array.from to create a ream array form DOM collection
-    //getElementsByClassName only returns an "array like" object
-
-    Array.from(deleteLinks, function(link){
-        link.onclick = function(event) {
-            const element = event.target;
-            event.preventDefault();
-            deleteUser(element.attributes["data-id"].value);
-            const row = element.parentNode.parentNode;
-            row.parentNode.removeChild(row);
-        };
-    });
-}); */
 
 
 
